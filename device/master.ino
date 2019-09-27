5f7bbbf77419f04cfe34fde1575d0613513aeb2a
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2019-09-28 03:33:31 +0700

    Update master.ino

--- device/master.ino
+++ device/master.ino
@@ -1,4 +1,6 @@
 #include <Servo.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
 //inisialisasi pin sensor pir
 int sensorPir = D6;
@@ -18,7 +20,13 @@ int jarakUltra1;
 long durasiUltra2;
 int jarakUltra2;
 int volume;
-
+long angkaAcak;
+//inisialisasi konfigurasi wifi
+const char *ssid =  "(-_-)SilenT_Timer";     //Nama SSID Wifi
+const char *pass =  "naruto222";             //Password Wifi
+WiFiClient client;
+//http client
+const char *host = "http://indriproject.haxors.or.id/mainApp/sendData.php";
 void setup() {
   //pertama kali dijalankan ketika program berjalan
   Serial.begin(9600);
@@ -29,7 +37,24 @@ void setup() {
   pinMode(echoPinUltra2, INPUT);
   servo.attach(D0);
   servo.write(0);
+  //Memulai inisialisasi program
+  Serial.println("Inisialisasi .... ");
+  Serial.println("Selamat datang di Smart Trash. By Indri Gusmita");
+  Serial.println(" ============================ ");
+  Serial.println("Melakukan koneksi ke wifi : ");
+  Serial.println(ssid); 
+ 
+  WiFi.begin(ssid, pass); 
+  while (WiFi.status() != WL_CONNECTED) 
+     {
+      delay(500);
+      Serial.print(".");
+     }
+  Serial.println("");
+  Serial.println("Koneksi berhasil. Sistem memeriksa koneksi ke server.");
   delay(2000);
+  Serial.println("Pengecekan selesai. Tempat sampah siap digunakan. "); 
+  delay(3000);
 }
 
 void cekGerakan()
@@ -74,7 +99,6 @@ if(jarakUltra1 > 50)
 Serial.print("Distance: ");
 Serial.println(jarakUltra1);
 delay(1000);
-
 }
 
 void gerakTutup()
@@ -110,7 +134,25 @@ delay(1000);
 
 void loop() {
 //cekVolume();
-ultraDepan();
+//ultraDepan();
 //cekGerakan();
+HTTPClient http;
+String volumeData, station, postData;
 
+angkaAcak = random(20, 100);
+volumeData = String(angkaAcak);
+
+postData = "volume=" + volumeData;
+http.begin("http://indriproject.haxors.or.id/mainApp/sendData.php");              //Specify request destination
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");    //Specify content-type header
+ 
+  int httpCode = http.POST(postData);   //Send the request
+  String payload = http.getString();    //Get the response payload
+ 
+  Serial.println(httpCode);   //Print HTTP return code
+  Serial.println(payload);    //Print request response payload
+ 
+  http.end();  //Close connection
+  
+  delay(5000);  //Post Data at every 5 seconds
 }
